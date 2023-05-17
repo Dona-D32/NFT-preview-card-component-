9d2cd639fe9fd77250454e360e54457eb367e332
Dona_D32 <dodadi@hotmail.ca>
2023-05-17 09:26:21 -0400

    first commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import nft_img from "../assets/images/image-equilibrium.jpg";
+import icon_eth from "../assets/images/icon-ethereum.svg";
+import icon_clock from "../assets/images/icon-clock.svg";
+import avatar_img from "../assets/images/image-avatar.png";
+
+const Card = ({ title, description, ethPrice, timeLeft, creatorName }) => {
+ /* On définit notre props dans l'argument de la fonction. */
+  const handleLike = () => {
+    console.log('Liked!');
+    // Ajoutez votre logique de gestion du "Like" ici
+  
+  };
+
+  const handleDislike = () => {
+    console.log('Disliked!');
+    // Ajoutez votre logique de gestion du "Dislike" ici
+  };
+
+  
+  return (
+    <div className="card">
+      <div className="image">
+        <img src={nft_img} alt="" />
+      </div>
+      <div className="details">
+        <a href="#">
+          <h3 className="title">Equilibrium #3429</h3>
+        </a>
+        <p className="description">
+          Our Equilibrium collection promotes balance and calm.
+        </p>
+        <div className="numbers">
+          <div className="price">
+            <img src={icon_eth} className="icon" />
+            <span>0.041 ETH</span>
+          </div>
+          <div className="duration">
+            <img src={icon_clock} className="icon" />
+            <span>3 days left</span>
+          </div>
+        </div>
+      </div>
+      <div className="creator">
+        <img src={avatar_img} />
+        <p className="creator-name">
+          Creation of
+          <a href="#"> Jules Wyvern</a>
+        </p>
+      </div>
+      <button className="button" onClick={handleLike}>
+        Like !
+      </button>
+      <button className="button" onClick={handleDislike}>
+        Dislike!
+      </button>
+    </div>
+
+    
+  );
+};
+
+export default Card;
